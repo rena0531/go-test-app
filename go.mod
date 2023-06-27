55232e6442b8d6bc19fe354cda9e371cc579fdc7
Rena <choco.rena0609@gmail.com>
2023-06-27 11:55:19 +0900

    Add: test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rena0531/go-test-app
+
+go 1.19
---
+++ main_test.go
@@ -0,0 +1,10 @@
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+    result := Add(1, 2)
+    if result != 3 {
+        t.Errorf("Add(1, 2) = %d; want 3", result)
+    }
+}
