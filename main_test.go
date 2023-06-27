207b20f44001c3136faa6ff7774b8f7ccac8e49f
Rena <choco.rena0609@gmail.com>
2023-06-27 14:07:26 +0900

    Add: sub test

--- main_test.go
+++ main_test.go
@@ -8,3 +8,10 @@ func TestAdd(t *testing.T) {
         t.Errorf("Add(1, 2) = %d; want 3", result)
     }
 }
+
+func TestSub(t *testing.T) {
+	result := Sub(2, 1)
+	if result != 1 {
+			t.Errorf("Sub(2, 1) = %d; want 1", result)
+	}
+}
